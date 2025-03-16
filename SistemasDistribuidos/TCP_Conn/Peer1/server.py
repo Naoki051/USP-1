195dfdfae189b2ccf9c204022d85ce33d57cdb57
NaokiTeruyaSI <152322175+NaokiTeruyaSI@users.noreply.github.com>
2025-03-16 15:36:26 -0300

    adicionando funcionalidades

--- SistemasDistribuidos/TCP_Conn/Peer1/server.py
+++ SistemasDistribuidos/TCP_Conn/Peer1/server.py
@@ -1,22 +1,5 @@
-import socket
-from client import separar_msg,incrementa_clock
-
-def dict_vizinhos(nome_arquivo='vizinhos.txt'):
-    vizinho = []
-    try:
-        with open(nome_arquivo, 'r') as arquivo:
-            for linha in arquivo:
-                linha = linha.strip()
-                if linha:
-                    ip, port = linha.split(':')
-                    vizinho.append({
-                        'ip': ip,
-                        'port': int(port),
-                        'status': 'OFFLINE'
-                    })
-    except FileNotFoundError:
-        print(f"Erro: Arquivo '{nome_arquivo}' não encontrado.")
-    return vizinho
+import socket, os
+from client import separar_msg,incrementa_clock,dict_vizinhos
 
 def servidor(data):
     """ Inicia um servidor TCP que aceita conexões de um único peer """
@@ -25,38 +8,41 @@ def servidor(data):
         servidor_socket.bind((data['ip'], data['port']))
         while True:
             servidor_socket.listen(1)
-            print(f"Servidor aguardando conexões em {data['ip']}:{data['port']}...")
-
             conexao, endereco = servidor_socket.accept()
             with conexao:
-                print(f"Conexão estabelecida com {endereco}")
                 try:
                     mensagem = conexao.recv(1024).decode()
-                    if not mensagem:
-                        print("Mensagem vazia!")
-                        # Encerra se a conexão for fechada
-                    print(f"Recebido: {mensagem}")
-                    mensagem = separar_msg(mensagem)
-                    incrementa_clock(data,mensagem['origem_clock'])
-                    if mensagem['tipo'].upper() == "SAIR":
+                    if mensagem.upper() == "SAIR":
                         print("Encerrando conexão servidor...")
-                        conexao.sendall("SAIR".encode())  # Confirma o encerramento
                         break
-                    resposta = f"{data['ip']}:{data['port']} {data['clock']} PEER_LIST {len(data['vizinhos'])} "
-                    for vizinho in data['vizinhos']:
-                        resposta += f"{vizinho['ip']}:{vizinho['port']}:{vizinho['status']}:0 "
-                    conexao.send(resposta.encode())
+                    print(f"Mensagem recebida: {mensagem}")
+                    mensagem = separar_msg(mensagem)
+
+                    if mensagem['tipo'].upper() == "HELLO":
+                        atualizar_status_vizinho(data,mensagem['origem_ip'],mensagem['origem_port'],"ONLINE")
+                    elif mensagem['tipo'].upper() == "BYE":
+                        atualizar_status_vizinho(data, mensagem['origem_ip'],mensagem['origem_port'],"OFFLINE")
+                    elif mensagem['tipo'].upper() == "GET_PEERS":
+                        resposta = f"{data['ip']}:{data['port']} {data['clock']} PEER_LIST {len(data['vizinhos'])-1} "
+                        for vizinho in data['vizinhos']:
+                            if vizinho['ip'] != mensagem['origem_ip'] or vizinho['port'] != mensagem['origem_port']:
+                                resposta += f"{vizinho['ip']}:{vizinho['port']}:{vizinho['status']}:0 "
+                        incrementa_clock(data)
+                        conexao.send(resposta.encode())
+                    
                 except Exception as e:
-                    print(f"Erro na conexão: {e}")
+                    print(f"Erro na conexão.")
                 
     print(f"Servidor finalizado.")
 
-data = {
-    "ip": "127.0.0.1",
-    "port": 9009,
-    "clock": 0,
-    "files_path":'./arquivos',
-    'vizinhos': []
-}
-
-servidor(data)
+def atualizar_status_vizinho(data, ip, port, status):
+    """Atualiza o status do vizinho para status se o IP:PORT corresponder."""
+    vizinho_encontrado = False  # Flag para verificar se algum vizinho foi encontrado
+    for index, vizinho in enumerate(data['vizinhos']):
+        if vizinho['ip'] == ip and vizinho['port'] == port:
+            if vizinho['status'] != status:
+                vizinho['status'] = status
+                print(f"Status do vizinho {vizinho['ip']}:{vizinho['port']} atualizado para {status}.")
+            vizinho_encontrado = True  # Define a flag como True se um vizinho for encontrado
+    if not vizinho_encontrado:
+        print(f"Nenhum vizinho encontrado com o IP:PORT {ip}:{port}.")
