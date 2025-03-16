b3c2f0c08bb73de22ff2c2cd24459b9310fdbddf
NaokiTeruyaSI <152322175+NaokiTeruyaSI@users.noreply.github.com>
2025-03-16 17:50:27 -0300

    atualizando peer 1

--- SistemasDistribuidos/TCP_Conn/Peer1/server.py
+++ SistemasDistribuidos/TCP_Conn/Peer1/server.py
@@ -17,21 +17,22 @@ def servidor(data):
                         break
                     print(f"Mensagem recebida: {mensagem}")
                     mensagem = separar_msg(mensagem)
+                    incrementa_clock(data,mensagem['origem_clock'])
 
-                    if mensagem['tipo'].upper() == "HELLO":
-                        atualizar_status_vizinho(data,mensagem['origem_ip'],mensagem['origem_port'],"ONLINE")
-                    elif mensagem['tipo'].upper() == "BYE":
+                    atualizar_status_vizinho(data,mensagem['origem_ip'],mensagem['origem_port'],"ONLINE")
+                    if mensagem['tipo'].upper() == "BYE":
                         atualizar_status_vizinho(data, mensagem['origem_ip'],mensagem['origem_port'],"OFFLINE")
                     elif mensagem['tipo'].upper() == "GET_PEERS":
                         resposta = f"{data['ip']}:{data['port']} {data['clock']} PEER_LIST {len(data['vizinhos'])-1} "
                         for vizinho in data['vizinhos']:
                             if vizinho['ip'] != mensagem['origem_ip'] or vizinho['port'] != mensagem['origem_port']:
                                 resposta += f"{vizinho['ip']}:{vizinho['port']}:{vizinho['status']}:0 "
-                        incrementa_clock(data)
+                        incrementa_clock(data,mensagem['origem_clock'])
+                        print(f"Encaminhando resposta: {resposta}")
                         conexao.send(resposta.encode())
                     
                 except Exception as e:
-                    print(f"Erro na conexão.")
+                    print(f"Erro na conexão.{e}")
                 
     print(f"Servidor finalizado.")
 
@@ -45,4 +46,11 @@ def atualizar_status_vizinho(data, ip, port, status):
                 print(f"Status do vizinho {vizinho['ip']}:{vizinho['port']} atualizado para {status}.")
             vizinho_encontrado = True  # Define a flag como True se um vizinho for encontrado
     if not vizinho_encontrado:
-        print(f"Nenhum vizinho encontrado com o IP:PORT {ip}:{port}.")
+        novo_vizinho = {"ip": ip, "port": port, "status": status}
+        data['vizinhos'].append(novo_vizinho)
+        print(f"Adicionando novo peer {ip}:{port} status {status}")  # Print do vizinho adicionado
+        try:
+            with open("vizinhos.txt", "a") as arquivo:
+                arquivo.write(f"\n{ip}:{port}")
+        except Exception as e:
+            print(f"Erro ao adicionar vizinho ao arquivo: {e}")
